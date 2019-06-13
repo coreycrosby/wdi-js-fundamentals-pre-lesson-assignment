6ed11004b4c0780847345b7b00d3a36df5d906d7
Corey Crosby <ccc_13@live.ca>
2019-06-13 15:36:28 -0400

    completed intro to javascript

--- pacman-console.js
+++ pacman-console.js
@@ -1,1 +1,10 @@
-console.log('Hello, Pac-man!');
+const ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde'];
+
+console.log(`There are ${ghosts.length} ghosts.`);
+console.log(`Their names are: `);
+console.log(ghosts[0]);
+console.log(ghosts[1]);
+console.log(ghosts[2]);
+console.log(ghosts[3]);
+
+
